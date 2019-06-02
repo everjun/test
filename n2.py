8459fb5d618a1cbe949aabaa63c3f8a1334959ea
Ilya Tsygankov <tsygankov.itis@gmail.com>
2019-06-02 14:32:08 +0300

    multiprocessing task b, initial data in files

--- n2.py
+++ n2.py
@@ -1,3 +1,6 @@
+from multiprocessing import Process, Manager, Queue
+
+
 def insert_by_sorted_value(lst, val):
     i = 0
     while i < len(lst):
@@ -48,25 +51,68 @@ def task_a(lst, row_counts=100):
     return new_list
 
 
-def task_b(lst):
-    last_dt = None
-    new_list = []
+def task_b_for_day(lst, q):
+    last_dt = lst[0][0].date()
     day_list = []
     avg_sum = 0
     avg_count = 0
     for tm, val in lst:
-        if last_dt is None:
-            last_dt = tm.date()
-        elif last_dt != tm.date():
-            new_list.append((last_dt, day_list[0], day_list[-1], avg_sum / avg_count,
-                             get_median_from_sorted_list(day_list, avg_count)))
-            last_dt = tm.date()
-            avg_sum = 0
-            avg_count = 0
-            day_list = []
         avg_sum += val
         avg_count += 1
         insert_by_sorted_value(day_list, val)
-    new_list.append((last_dt, day_list[0], day_list[-1], avg_sum / avg_count,
-                     get_median_from_sorted_list(day_list, avg_count)))
+    q.put((last_dt, day_list[0], day_list[-1], avg_sum / avg_count,
+           get_median_from_sorted_list(day_list, avg_count)))
+
+
+def task_b(lst):
+    last_dt = None
+    manager = Manager()
+    new_list = manager.list([])
+    q = Queue()
+    procs = []
+    last_index = 0
+    for i, (tm, val) in enumerate(lst):
+        if last_dt is None:
+            last_dt = tm.date()
+        elif last_dt != tm.date():
+            last_dt = tm.date()
+            proc = Process(target=task_b_for_day, args=(lst[last_index:i], q))
+            proc.start()
+            procs.append(proc)
+            last_index = i
+    proc = Process(target=task_b_for_day, args=(lst[last_index:], q))
+    proc.start()
+    procs.append(proc)
+    for proc in procs:
+        proc.join()
+    while not q.empty():
+        new_list.append(q.get())
     return new_list
+
+
+if __name__ == '__main__':
+    import random
+    from datetime import datetime, timedelta
+
+    l = [(datetime(2019, 1, 1) + timedelta(seconds=k), k % 50) for k in range(1000)]
+    with open('initial_a.txt', 'w') as f:
+        f.write("time    value\n")
+        for time, value in l:
+            f.write("%s %s\n" % (time.strftime('%Y-%m-%d %H:%M:%S'), value))
+    with open('result_a.txt', 'w') as f:
+        f.write("time    value    min    max    avg    median\n")
+        for time, value, mn, mx, avg, median in task_a(l):
+            f.write("%s %s %s %s %s %s\n" % (time.strftime('%Y-%m-%d %H:%M:%S'), value, mn, mx, avg, median))
+    l = [(datetime(2019, 1, 1) + timedelta(seconds=k), k % 50) for k in range(60 * 60)]
+    l += [(datetime(2019, 1, 2) + timedelta(seconds=k), k % 40) for k in range(60 * 60)]
+    l += [(datetime(2019, 1, 3) + timedelta(seconds=k), k % 60) for k in range(60 * 60)]
+    with open('initial_b.txt', 'w') as f:
+        f.write("time    value\n")
+        for time, value in l:
+            f.write("%s %s\n" % (time.strftime('%Y-%m-%d %H:%M:%S'), value))
+    with open('result_b.txt', 'w') as f:
+        f.write("time    min    max    avg    median\n")
+        print('start counting task b')
+        for time, mn, mx, avg, median in task_b(l):
+            f.write("%s %s %s %s %s\n" % (time.strftime('%Y-%m-%d'), mn, mx, avg, median))
+        print('end counting task b')
